148cdf118dd826a5656b97ed2dfa2f7b504a031f
markodjukic28 <markogvs@gmail.com>
2024-11-04 21:31:34 +0100

    Pig Game App updated and finished

--- script.js
+++ script.js
@@ -1,91 +1,68 @@
-'use strict';
-
 // Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-
+const players = [document.querySelector('.player--0'), document.querySelector('.player--1')];
+const scoresEl = [document.getElementById('score--0'), document.getElementById('score--1')];
+const currentsEl = [document.getElementById('current--0'), document.getElementById('current--1')];
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const [btnNew, btnRoll, btnHold] = document.querySelectorAll('.btn');
 
+// Game variables
 let scores, currentScore, activePlayer, playing;
 
-// Starting conditions
+// Initialize game state
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
   playing = true;
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+  scoresEl.forEach(el => (el.textContent = 0));
+  currentsEl.forEach(el => (el.textContent = 0));
 
   diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  players.forEach(player => player.classList.remove('player--winner', 'player--active'));
+  players[0].classList.add('player--active');
 };
 init();
 
 const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  currentsEl[activePlayer].textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  players.forEach(player => player.classList.toggle('player--active'));
 };
 
-// Rolling dice functionality
+// Roll dice event
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
 
-    // 3. Check for rolled 1:
     if (dice !== 1) {
-      // Add dice to current score
-      currentScore = currentScore + dice;
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      currentScore += dice;
+      currentsEl[activePlayer].textContent = currentScore;
     } else {
-      // switch to next player
       switchPlayer();
     }
   }
 });
 
+// Hold score event
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // 1. Add current score to active player's score
-    scores[activePlayer] = scores[activePlayer] + currentScore;
-    // scores[1] = scores[1] + currentScore
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    scores[activePlayer] += currentScore;
+    scoresEl[activePlayer].textContent = scores[activePlayer];
 
-    // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
-      // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
-
-      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+      players[activePlayer].classList.add('player--winner');
+      players[activePlayer].classList.remove('player--active');
     } else {
-      // Switch to the next player
       switchPlayer();
     }
   }
 });
 
-// New game reset button
+// New game reset event
 btnNew.addEventListener('click', init);
